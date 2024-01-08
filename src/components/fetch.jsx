2683a256d48858be9c03b75e1b2b22499681f0bc
Jomerjom <ekenesjoachim@gmail.com>
2024-01-08 13:59:38 +0100

    Refactor Fetch component to handle input and currency changes

--- src/components/fetch.jsx
+++ src/components/fetch.jsx
@@ -2,9 +2,11 @@ import { useState, useEffect } from "react";
 
 export const Fetch = () => {
   const [data, setData] = useState(null);
-  const rates = data.rates;
-  const currencies = Object.keys(rates);
+  const rates = data?.rates;
+  const currencies = Object.keys(rates || {});
   const [convertedValue, setConvertedValue] = useState(0);
+  const [inputValue, setInputValue] = useState(0);
+  const [selectedCurrency, setSelectedCurrency] = useState("");
 
   useEffect(() => {
     fetch(
@@ -15,23 +17,47 @@ export const Fetch = () => {
       .catch((error) => console.error(error));
   }, []);
 
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+    const selectedRate = rates[selectedCurrency];
+    const convertedValue = (Number((event.target.value * selectedRate).toFixed(2)));
+    setConvertedValue(convertedValue);
+  };
+
+  const handleCurrencyChange = (event) => {
+    setSelectedCurrency(event.target.value);
+    const selectedRate = rates[event.target.value];
+    const convertedValue = (Number((inputValue * selectedRate).toFixed(2)));
+    setConvertedValue(convertedValue);
+  };
+
   return (
     <div className="flex flex-col gap-2 p-4 rounded bg-[#202124] w-fit text-[#bdc1c6]">
       <div className="flex gap-2 rounded border border-[#bdc1c6] px-4 py-2">
-        <input type="number" placeholder="0" className="bg-[#202124] flex w-20" />
+        <input
+          type="number"
+          placeholder="0"
+          className="bg-[#202124] flex w-20"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
         <vr className="bg-[#bdc1c6] w-[1px]" />
         <h1 className="flex font-bold">EUR</h1>
       </div>
       <div className="flex gap-2 rounded border border-[#bdc1c6] px-4 py-2">
-        <p className="flex w-20">0</p>
+        <p className="flex w-20">{convertedValue}</p>
         <vr className="bg-[#bdc1c6] w-[1px]" />
-      <select className="bg-[#202124] flex font-bold">
-        {currencies.map((currency, index) => (
-          <option key={index} value={currency}>
-            {currency}
-          </option>
-        ))}
-      </select>
+        <select
+          className="bg-[#202124] flex font-bold"
+          value={selectedCurrency}
+          onChange={handleCurrencyChange}
+        >
+          {currencies.map((currency, index) => (
+            <option key={index} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
       </div>
     </div>
   );
